dd63e23170203de7c68567624cde6e175cb8f22f
Christoph Pegel <rio@eta-ori.net>
2013-12-10 15:36:51 +0100

    implemented send to all

--- send.cpp
+++ send.cpp
@@ -10,14 +10,26 @@ using std::endl;
 using std::stoi;
 using std::string;
 
+void send(PT22x2::Encoder encoder, unsigned int system_code, unsigned int channel, Command::State state) {
+    Command cmd(system_code, channel, state);
+    auto codeword = cmd.getCodeword();
+    for (int i=0; i<5; i++) {
+        encoder.send(codeword);
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 4) {
-        cout << "usage: send <system_code> <channel> on|off" << endl;
+        cout << "usage: send <system_code> <channel>|all on|off" << endl;
         return -1;
     }
 
+    GPIOPin pin(24);
+    pin.setOutput();
+    PT22x2::Encoder encoder(pin);
+
     unsigned int system_code = stoi(argv[1]);
-    unsigned int channel     = stoi(argv[2]);
+
     Command::State state;
     if (argv[3] == string("on")) {
         state = Command::State::On;
@@ -30,17 +42,15 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    GPIOPin pin(24);
-    pin.setOutput();
-    PT22x2::Encoder encoder(pin);
-
-    Command cmd(system_code, channel, state);
-    auto codeword = cmd.getCodeword();
-    cout << cmd << endl;
-    cout << codeword << endl;
-    for (int i=0; i<5; i++) {
-        encoder.send(codeword);
+    if (argv[2] == string("all")) {
+        send(encoder, system_code, 1, state);
+        send(encoder, system_code, 2, state);
+        send(encoder, system_code, 4, state);
+        send(encoder, system_code, 8, state);
     }
-
+    else {
+        unsigned int channel = stoi(argv[2]);
+        send(encoder, system_code, channel, state);
+    }
     return 0;
 }
